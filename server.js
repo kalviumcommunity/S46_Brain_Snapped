b37ea3ef8d9d1777e1807acbe825783f8a467ab9
Snegan P <pssj2914@gmail.com>
2024-02-13 13:57:42 +0530

    Rechecking and creating a new pull request for mongodb-client.

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const mongoose = require ('mongoose')
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 1000;
+const port = 3000;
 
 const dataBase = async () => {
-    mongoose.connect(process.env.URL)
+    mongoose.connect(process.env.LINK)
 }
 dataBase()
 
