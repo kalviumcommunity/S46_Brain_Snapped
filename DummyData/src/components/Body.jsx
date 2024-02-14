1c6635e1586282688c5731fb1e9b39cb6aa5bb43
Snegan P <pssj2914@gmail.com>
2024-02-14 12:10:31 +0530

    Made a functional dummy data.

---
+++ DummyData/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RandomThingsGenerator from './RandomThingsGenerator'
+
+function Body() {
+  return (
+    <div className='random-thing-generator'>
+        <RandomThingsGenerator/>
+    </div>
+  )
+}
+
+export default Body
